fb0ae2dea0d74c60b0c2bd8d86be8feaad4cdc0b
Khornne <maurice.pattinsonn@gmail.com>
2025-06-05 21:25:55 +0100

    Updated player draw

--- sources/main.c
+++ sources/main.c
@@ -5,10 +5,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// DEFINES
+#define TILE_SIZE 55
+
+// GLOBALS
 const int screenWidth = 1024;
 const int screenHeight = 512;
-int map_x = 8, map_y = 8, map_s = 64;
-Vector2 playerPosition = {(float)screenWidth / 2, (float)screenHeight / 2};
 
 const uint8_t map[10][10] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, {1, 0, 1, 0, 0, 0, 0, 0, 0, 1},
@@ -17,8 +19,6 @@ const uint8_t map[10][10] = {
     {1, 0, 1, 1, 0, 0, 1, 0, 0, 1}, {1, 0, 1, 0, 0, 0, 1, 1, 1, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 1}, {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 };
-
-#define TILE_SIZE 55
 
 void display_map2D(const uint8_t map[10][10]) {
   for (uint8_t row = 0; row < 10; row++) {
@@ -33,18 +33,31 @@ void display_map2D(const uint8_t map[10][10]) {
   }
 }
 
-void display_player2D() { DrawCircleV(playerPosition, 5, RED); }
+void display_player2D(const Vector2 position) {
+  DrawCircle(position.x * TILE_SIZE + TILE_SIZE / 2,
+             position.y * TILE_SIZE + TILE_SIZE / 2, 6, RED);
+};
 
 // Player movement
 void player_movement() {
-  if (IsKeyDown(KEY_W))
-    playerPosition.y -= 2.0f;
-  if (IsKeyDown(KEY_S))
-    playerPosition.y += 2.0f;
-  if (IsKeyDown(KEY_A))
-    playerPosition.x -= 2.0f;
-  if (IsKeyDown(KEY_D))
-    playerPosition.x += 2.0f;
+  double playerX = 2.0;
+  double playerY = 2.0;
+  double player_angle = 0;
+
+  if (IsKeyDown(KEY_W)) {
+    playerX += cos(player_angle) * 0.01;
+    playerY += sin(player_angle) * 0.01;
+  }
+  if (IsKeyDown(KEY_S)) {
+    playerX -= cos(player_angle) * 0.01;
+    playerY -= sin(player_angle) * 0.01;
+  }
+  if (IsKeyDown(KEY_A)) {
+    player_angle -= 0.5;
+  }
+  if (IsKeyDown(KEY_D)) {
+    player_angle += 0.5;
+  }
 };
 
 // Initialise and Draws window
@@ -54,13 +67,14 @@ int main(void) {
   InitWindow(screenWidth, screenHeight, "Raycaster");
 
   while (!WindowShouldClose()) {
+    Vector2 playerPosition = (Vector2){1, 1};
     player_movement();
     BeginDrawing();
 
     ClearBackground(GRAY);
 
     display_map2D(map);
-    display_player2D();
+    display_player2D(playerPosition);
     EndDrawing();
   }
   CloseWindow();
