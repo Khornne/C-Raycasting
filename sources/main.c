fcb4b96f9508f1a4c3fb4cb071653f7d2545fcd3
Khornne <maurice.pattinsonn@gmail.com>
2025-06-08 15:41:27 +0100

    Window Initialisation

--- sources/main.c
+++ sources/main.c
@@ -23,4 +23,21 @@ int main() {
   double playerX = 2.0, playerY = 2.0; // x and y start postition
   double dirX = -1, dirY = 0;          // intial direction vector
   double planeX = 0, planeY = 0.66;    // 2d raycaster version of camera plane
+
+  double time = 0;    // time of current frame
+  double oldTime = 0; // time of previous frame
+
+  printf("Starting Game....\n");
+
+  InitWindow(screenWidth, screenHeight, "Raycaster");
+  SetTargetFPS(60);
+
+  // Main Loop
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(GRAY);
+    EndDrawing();
+  }
+  CloseWindow();
+  return 0;
 }
