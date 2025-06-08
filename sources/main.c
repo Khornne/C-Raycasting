a4579a8109f0460f124371d80b5bb40aa2622c5e
Khornne <maurice.pattinsonn@gmail.com>
2025-06-08 15:35:58 +0100

    Refractor Code, Created Map

--- sources/main.c
+++ sources/main.c
@@ -6,77 +6,21 @@
 #include <stdlib.h>
 
 // DEFINES
-#define TILE_SIZE 55
+#define mapWidth 10
+#define mapHeight 10
+#define screenWidth 1280
+#define screenHeight 720
 
-// GLOBALS
-const int screenWidth = 1024;
-const int screenHeight = 512;
-
-const uint8_t map[10][10] = {
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, {1, 0, 1, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 1, 0, 0, 0, 0, 0, 0, 1}, {1, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 1, 0, 0, 0, 0, 1}, {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 1, 1, 0, 0, 1, 0, 0, 1}, {1, 0, 1, 0, 0, 0, 1, 1, 1, 1},
+int worldMap[mapWidth][mapHeight] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, {1, 0, 2, 0, 1, 0, 0, 0, 1, 1},
+    {1, 0, 0, 0, 1, 0, 0, 0, 0, 1}, {1, 2, 2, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1}, {1, 0, 0, 0, 0, 3, 0, 3, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1}, {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 1}, {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 };
 
-void display_map2D(const uint8_t map[10][10]) {
-  for (uint8_t row = 0; row < 10; row++) {
-    for (uint8_t col = 0; col < 10; col++) {
-      Color color = BLACK;
-      if (map[row][col] == 1) {
-        color = WHITE;
-      };
-      DrawRectangle(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE,
-                    color);
-    };
-  }
-}
-
-void display_player2D(const Vector2 position) {
-  DrawCircle(position.x * TILE_SIZE + TILE_SIZE / 2,
-             position.y * TILE_SIZE + TILE_SIZE / 2, 6, RED);
-};
-
-// Player movement
-void player_movement() {
-  double playerX = 2.0;
-  double playerY = 2.0;
-  double player_angle = 0;
-
-  if (IsKeyDown(KEY_W)) {
-    playerX += cos(player_angle) * 0.01;
-    playerY += sin(player_angle) * 0.01;
-  }
-  if (IsKeyDown(KEY_S)) {
-    playerX -= cos(player_angle) * 0.01;
-    playerY -= sin(player_angle) * 0.01;
-  }
-  if (IsKeyDown(KEY_A)) {
-    player_angle -= 0.5;
-  }
-  if (IsKeyDown(KEY_D)) {
-    player_angle += 0.5;
-  }
-};
-
-// Initialise and Draws window
-
-int main(void) {
-  SetTargetFPS(60);
-  InitWindow(screenWidth, screenHeight, "Raycaster");
-
-  while (!WindowShouldClose()) {
-    Vector2 playerPosition = (Vector2){1, 1};
-    player_movement();
-    BeginDrawing();
-
-    ClearBackground(GRAY);
-
-    display_map2D(map);
-    display_player2D(playerPosition);
-    EndDrawing();
-  }
-  CloseWindow();
-  return 0;
+int main() {
+  double playerX = 2.0, playerY = 2.0; // x and y start postition
+  double dirX = -1, dirY = 0;          // intial direction vector
+  double planeX = 0, planeY = 0.66;    // 2d raycaster version of camera plane
 }
