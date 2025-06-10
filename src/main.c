ede81bf63a471c09715491eccb0395fc82f95357
Khornne <maurice.pattinsonn@gmail.com>
2025-06-10 16:12:53 +0100

    Created Player Movment

--- src/main.c
+++ src/main.c
@@ -6,14 +6,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float playerX, playerY; // player position
+
+// Function to draw the Player
+void drawPlayer() {
+  glColor3f(1, 1, 0);
+  glPointSize(8);
+  glBegin(GL_POINTS);
+  glVertex2i(playerX, playerY);
+  glEnd();
+}
+
+// Function to update display
 void display() {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  drawPlayer();
   glutSwapBuffers();
 }
 
+// Player Movement
+void playerMvmt(unsigned char key, int x, int y) {
+  if (key == 'w') {
+    playerY -= 5;
+  }
+  if (key == 's') {
+    playerY += 5;
+  }
+  if (key == 'a') {
+    playerX -= 5;
+  }
+  if (key == 'd') {
+    playerX += 5;
+  }
+  glutPostRedisplay();
+}
+// Initalise function
 void init() {
   glClearColor(0.3, 0.3, 0.3, 0);
   gluOrtho2D(0, 1024, 512, 0);
+  playerX = 300;
+  playerY = 300;
 }
 
 int main(int argc, char *argv[]) {
@@ -23,5 +55,6 @@ int main(int argc, char *argv[]) {
   glutCreateWindow("Raycaster");
   init();
   glutDisplayFunc(display);
+  glutKeyboardFunc(playerMvmt);
   glutMainLoop();
 }
